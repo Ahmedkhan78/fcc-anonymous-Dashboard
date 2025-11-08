e2538b8b9620fd60bc5119437282bcd7329d5c89
Ahmedkhan78 <ahmedhasan9619@gmail.com>
2025-11-08 21:21:32 +0530

    Update fix

--- server.js
+++ server.js
@@ -25,6 +25,16 @@ app.use(cors({ origin: "*" })); // For FCC testing only
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use((req, res, next) => {
+  // Only allow iframe from same origin
+  res.setHeader("X-Frame-Options", "SAMEORIGIN");
+  // Disable DNS prefetching
+  res.setHeader("X-DNS-Prefetch-Control", "off");
+  // Referrer policy
+  res.setHeader("Referrer-Policy", "same-origin");
+  next();
+});
+
 // Frontend pages
 app.route("/b/:board/").get((req, res) => {
   res.sendFile(process.cwd() + "/views/board.html");
